95c9d81952ac1862bcde20339c11ea3d715144be
Raquel <raquelsweeta@gmail.com>
2023-11-02 12:57:04 +0100

    ✨ (react native course) - my first hello world with it style

--- App.tsx
+++ App.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import { Text, View } from 'react-native';
 
 export const App = () => {
+
+
   return (
-    <View>
-      <Text>
+    <View style={{
+          flex: 1,
+          justifyContent: 'center',
+    }}>
+      <Text style={{
+        fontSize: 30,
+        textAlign: 'center',
+
+      }}>
         Hola Mundo
       </Text>
     </View>
-  )
-}
+  );
+};
 
 export default App;
